4cf0e5323f64da4d79156ded877d619c2296ad01
sirYofat <siryofat@gmail.com>
2024-04-01 23:44:43 +0200

    kata: solved greet function

---
+++ greet.py
@@ -0,0 +1,21 @@
+'''
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+'''
+
+def greet(name: str, owner: str) -> str:
+    '''
+
+    :param name: Person to greet
+    :param owner: Person who owns?
+    :return: A greeting
+    '''
+    return "Hello boss" if name == owner else "Hello guest"
+
+print(greet("hi", "hi2"))
