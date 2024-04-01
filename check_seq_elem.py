58652da1ed78de4d3bfa1f67b6b31b0054786066
sirYofat <siryofat@gmail.com>
2024-04-02 00:05:26 +0200

    kata: solved check for element in sequence

---
+++ check_seq_elem.py
@@ -0,0 +1,16 @@
+'''
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+'''
+
+def check(seq: list, elem: [str | int]) -> bool:
+    '''
+    Checks if element exist in giben sequence
+    :param seq: list to check
+    :param elem: element to look for.
+    :return: True if element exists in sequence, else False.
+    '''
+    return True if elem in seq else False
